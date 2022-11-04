7894b6b89f12fb9c80f9649b88f3ecd667d30238
damianparchi <damian.parchimowicz99@gmail.com>
2022-11-04 22:32:09 +0100

    poprawki inputów i validacja w angular

--- frontend-ksiegarnia/src/app/Components/register/register.component.ts
+++ frontend-ksiegarnia/src/app/Components/register/register.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UserServiceService} from "../../Services/user-service.service";
 import {User} from "../../Services/user";
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,18 +9,47 @@ import {User} from "../../Services/user";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  user:User =new User();
+  email = new FormControl('', [Validators.required, Validators.email]);
+  mobileNumber = new FormControl('', [Validators.required, Validators.pattern(new RegExp("[2-8 ]{8}"))]);
+  username = new FormControl('', [Validators.required])
+  hide = true;
+
+  user: User = new User();
+
   constructor(private userServiceService: UserServiceService
   ) {
   }
-  ngOnInit(): void{
+
+  ngOnInit(): void {
   }
 
-  userRegister(){
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Podaj email';
+    }
+    return this.email.hasError('email') ? 'Niepoprawny email' : '';
+  }
+
+  getErrorMobileMessage() {
+    if (this.mobileNumber.hasError('required')) {
+      return 'Podaj nr tel';
+    }
+    return this.mobileNumber.hasError('pattern') ? 'Zły numer' : '';
+  }
+
+  getErrorNameMessage() {
+    if (this.mobileNumber.hasError('required')) {
+      return 'Podaj nazwę użytkownika';
+    }
+    return this.mobileNumber.hasError('pattern') ? 'Zła nazwa użytkownika' : '';
+
+  }
+
+  userRegister() {
     console.log(this.user);
-    this.userServiceService.registerUser(this.user).subscribe(data=>{
+    this.userServiceService.registerUser(this.user).subscribe(data => {
       alert("Zarejestrowano pomyślnie!")
-    },error=>alert("Nie udało się zarejestrować!"));
+    }, error => alert("Nie udało się zarejestrować!"));
   }
 
 
