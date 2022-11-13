b1770dd727db741abed46fd2eca519227a40e8cf
damianparchi <damian.parchimowicz99@gmail.com>
2022-11-13 14:59:34 +0100

    serwis dla admina

---
+++ frontend-ksiegarnia/src/app/Services/admin-service.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpServiceService} from "./http-service.service";
+import {Observable, Subject, tap} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminServiceService {
+
+  private baseUrl = 'http://localhost:8080';
+  private subject = new Subject<any>();
+  public get autoRefresh() {
+    return this.subject;
+  }
+
+  constructor(private http: HttpClient, private httpService: HttpServiceService) { }
+
+  private httpOptions = {
+    headers: new HttpHeaders ({'content-type': 'application/json' , token: localStorage?.['token']})
+  };
+
+  getUnconfirmedBooks(status: string) {
+    return this.httpService.get(this.baseUrl + "/admin/books" + '?status=' + status, this.httpOptions);
+  }
+
+  confirmBooks(bookId: number, status: string): Observable<any> {
+    return this.httpService.put(this.baseUrl + "/admin/confirmBook/" + bookId + '?' + 'status=' + status, '', this.httpOptions);
+  }
+
+  deniedBooks(bookId: number, status: string): Observable<any> {
+    return this.httpService.put(this.baseUrl + "/admin/confirmBook/" + bookId + '?' + 'status=' + status, '', this.httpOptions);
+  }
+
+}
