4559e7457515f7e3484f141f5e77e6a4b57f00ad
damianparchi <damian.parchimowicz99@gmail.com>
2022-11-18 10:43:42 +0100

    relacja encji dla usera z itemami w koszyku

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Data
@@ -20,5 +21,9 @@ public class User {
     private LocalDateTime createdDate;
     private boolean isVerified;
     private String role;
+
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = BasketProduct.class)
+    @JoinColumn(name = "userId")
+    private List<BasketProduct> BooksInBasket;
 
 }
